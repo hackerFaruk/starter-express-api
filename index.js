33c5a33b42fae460855e3be37708097e5b1b13ba
hackerFaruk <onuralparslan97@gmail.com>
2024-02-08 23:59:04 +0300

    updated name

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const app = express()
 
+
+
 // all answers all html methods 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
 
+// random Name 
+
+function getRandomName(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+}
+
+// names
+const myNames = ['hackerFaruk', 'Onuralp', 'Parancey', 'Onuralp Arslan'];
+
 // get answers get html method
 app.get('/name', (req, res) => {
-    res.send('Parancey');
+    res.send(getRandomName(myNames));
 });
 
 
