7cf8f0238e2fb10ea8938b271c89100e585b14a3
hackerFaruk <onuralparslan97@gmail.com>
2024-02-08 23:55:12 +0300

    added new method

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
+
+// all answers all html methods 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+// get answers get html method
+app.get('/name', (req, res) => {
+    res.send('Parancey');
+});
+
+
+
 app.listen(process.env.PORT || 3000)
